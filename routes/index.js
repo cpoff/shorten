684ebac09dbab0b4af0bdcdc026f8dcfba435e02
[angela riggs] <[riggs.ang@gmail.com]>
2015-06-11 09:18:02 -0700

    updated post

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var uuid = require('node-uuid');
-var target = request.body.URL
+
+var longUrl;
+// var target = request.body.URL
 
 /*Connects w/MongoDB server*/
 MongoClient.connect('mongodb://127.0.0.1:27017/shorten', function(err, db) {
@@ -26,36 +28,41 @@ router.get('/', function(request, response) {
 router.post('/', function(request, response) {
   var shortKey = uuid.v4();
 
-  var countClicks = function() {
+  
 
   var shorten_url = db.collection('shorten_url');
   shorten_url.insert(
     {"_id": _id,
     "shortened": shortKey,
     "target": "https://google.com",
-    "clicks": 8,
+    "clicks": clicks,
     "last_click": "2015-01-13T16:42:00"}, function(err, docs) {
     response.redirect('/info/' + shortKey);
   });//closes .insert
 });//closes route
 
 
-router.get('/info/:shortUrl', function(request, response) {
-
+router.get('/post/:shortUrl', function(request, response) {
+  var countClicks = function() { 
   db.shorten_url.find(
-    {"clicks": 1, _id: 0
-    });//closes .find
+  {"shortened": request.param.shortUrl},
+  function(err, results) {
+    console.log(results);
+  }//closes function
+  );//closes .find
 
-    db.shorten_url.update(
-    {$inc {"clicks": 1}
-    })//closes .update
+  //   db.shorten_url.update(
+  //   {$inc {"clicks": 1}
+  //   })//closes .update
+  // };
 
   var collection = db.collection('urls'),
       shortUrl = request.params.shortUrl;
   collection.find({'shortened': shortUrl}, function(err, url) {
     response.render('info', {url: url});
-  });
-});
+  });//closes collection.find
+  }//closes var countClicks
+});//closes router.get
 
 router.get('/:shortUrl', function(request, response) {
   var collection = db.collection('urls'),
