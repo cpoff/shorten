f1608f2bd57f590bbf2cf19fd3a6cd92093b74e9
[angela riggs] <[riggs.ang@gmail.com]>
2015-06-11 14:46:33 -0700

    white space and date

--- routes/index.js
+++ routes/index.js
@@ -1,81 +1,87 @@
 var express = require('express');
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
 var uuid = require('node-uuid');
 
+var MongoClient = require('mongodb').MongoClient;
 var longUrl;
-// var target = request.body.URL
+var clicks;
+var date = new Date()
 
-/*Connects w/MongoDB server*/
-MongoClient.connect('mongodb://127.0.0.1:27017/shorten', function(err, db) {
-  if (err) {
-    throw err;
-  }//closes if
-})//closes MongoClient.connect
 
 /* GET home page. */
-router.get('/', function(request, responds, next) {
-  response.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.render('index');
 });
 
-/**/
-router.get('/', function(request, response) {
-  response.render('index', {});
-  // index.jade needs a form to submit a URL for shortening
-});
+MongoClient.connect('mongodb://127.0.0.1:27017/shorten', function(err, db) {
 
-/*Redirects to the page*/
-router.post('/', function(request, response) {
+  router.post('/', function(request, response) {
+
   var shortKey = uuid.v4();
+  var target= request.body.URL
+  console.log("howdy")
 
-  
+  var collection =  db.collection('shorten_url')
+    collection.find(
+      {'target': {$eq: target}},
+      { target: 1, shortened:1}
+      )
+      .toArray(function(err,reply){
+        if (reply.length>0){
+          console.log(target)
+          console.log(reply)
+          response.redirect(reply[0].shortened);
+        } else {
+          collection.insert({
+            "shortened": shortKey,
+            "target": target,
+            "clicks": 0,
+            "last_click": new Date()}, function(err, docs) {
+              collection.find().toArray(function(err, results) {
+                response.redirect(shortKey);
+              });// closes collection.find
+              }// closes function
+          )// closes collection.inser;
+          }// closes else
+      })// closes .toArray
+  });// closes router.pose  
 
-  var shorten_url = db.collection('shorten_url');
-  shorten_url.insert(
-    {"_id": _id,
-    "shortened": shortKey,
-    "target": "https://google.com",
-    "clicks": clicks,
-    "last_click": "2015-01-13T16:42:00"}, function(err, docs) {
-    response.redirect('/info/' + shortKey);
-  });//closes .insert
-});//closes route
 
+  router.get('/:shortUrl', function(request, response){
+    console.log('here') 
+    var collection = db.collection('shorten_url')
+    var shortUrl = request.params.shortUrl;
 
-router.get('/post/:shortUrl', function(request, response) {
-  var countClicks = function() { 
-  db.shorten_url.find(
-  {"shortened": request.param.shortUrl},
-  function(err, results) {
-    console.log(results);
-  }//closes function
-  );//closes .find
+    collection.find(
+      {shortened: {$eq: shortUrl}},
+      { target: 1, shortened: 1, clicks:1, last_click:1}
+    )
+    .toArray(function(err, result){
+      response.render('post', {"url": result[0]});
+    })// closes .toArray
+  });// closes router.get
 
-  //   db.shorten_url.update(
-  //   {$inc {"clicks": 1}
-  //   })//closes .update
-  // };
+  router.get('/goto/:shortUrl', function(request, response){
+    var collection = db.collection('shorten_url')
+    var shortUrl = request.params.shortUrl;
+    collection.update(
+      {shortened: {$eq: shortUrl}},
+      {
+        $inc: {clicks: 1},
+        $set: {last_click: new Date()}
+      }
+    )// closes collection.update
+           
+    collection.find(
+      {shortened: {$eq: shortUrl}},
+      { target: 1, shortened: 1, clicks:1, last_click:1}
+    )
+    .toArray(function(err, result){
+      response.redirect("http://"+result[0].target)
+    })
 
-  var collection = db.collection('urls'),
-      shortUrl = request.params.shortUrl;
-  collection.find({'shortened': shortUrl}, function(err, url) {
-    response.render('info', {url: url});
-  });//closes collection.find
-  }//closes var countClicks
-});//closes router.get
-
-router.get('/:shortUrl', function(request, response) {
-  var collection = db.collection('urls'),
-      shortUrl = request.params.shortUrl;
-  collection.find({'shortened': shortUrl}, function(err, url) {
-    response.redirect(url.target);
-  });
-});
+  });// closes router.get
+});// closes MongoClient
 
 
 module.exports = router;
-
-
-
-
-
