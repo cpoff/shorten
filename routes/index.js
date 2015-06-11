da7a911fc02d1fcbb70efc46e2576875b316bd6e
[angela riggs] <[riggs.ang@gmail.com]>
2015-06-11 14:31:08 -0700

    routes

--- routes/index.js
+++ routes/index.js
@@ -11,78 +11,65 @@ var clicks;
 router.get('/', function(req, res, next) {
  res.render('index');
 });
+
 MongoClient.connect('mongodb://127.0.0.1:27017/shorten', function(err, db) {
-router.post('/', function(request, response) {
-var shortKey = uuid.v4();
-var target= request.body.URL
-console.log("howdy")
 
- var collection =  db.collection('shorten_url')
- collection.find(
-       {'target': {$eq: target}},
-       { target: 1, shortened:1}
-       ).toArray(function(err,reply){
-         if (reply.length>0){
-           console.log(target)
-           console.log(reply)
-           response.redirect(reply[0].shortened);
-         }else{
-             collection.insert({
-                         "shortened": shortKey,
-                         "target": target,
-                         "clicks": 8,
-                         "last_click": "2015-01-13T16:42:00"}, function(err, docs) {
-                             collection.find().toArray(function(err, results) {
-                             console.dir(results);
-                            
-                           response.redirect(shortKey);
+  router.post('/', function(request, response) {
+    var shortKey = uuid.v4();
+    var target= request.body.URL
+    console.log("howdy")
+    var date = ISODate();
 
-               });
-             });
-         }
-       })
+    var collection =  db.collection('shorten_url')
+    collection.find(
+      {'target': {$eq: target}},
+      { target: 1, shortened:1}
+      ).toArray(function(err,reply){
+        if (reply.length>0){
+          console.log(target)
+          console.log(reply)
+          response.redirect(reply[0].shortened);
+        } else {
+          collection.insert({
+            "shortened": shortKey,
+            "target": target,
+            "clicks": 0,
+            "last_click": date}, function(err, docs) {
+            collection.find().toArray(function(err, results) {
+              console.dir(results);
+              response.redirect(shortKey);
+            });// closes collection.find function
+            }// closes function
+          );// closes collection.insert
+        }// closes else
+    })// closes toArray
+  });// closes router.post  
 
-});  
 
+  router.get('/:shortUrl', function(request, response){
+    console.log('here')
+    var collection = db.collection('shorten_url')
+    var shortUrl = request.params.shortUrl;
+    
+    collection.update(
+      {shortened: {$eq: shortUrl}},
+      {$inc: {clicks: 1}}
+    )//closes collection.update
+    
+    collection.find(
+      {shortened: {$eq: shortUrl}},
+      {target:1, shortened:1, clicks:1, last_click:1}
+    )//closes collection.find
+    .toArray(function(err, result){
+      console.log(result)
+      response.render('post', {"url": result[0]});
+   }) // closes .toArray
+  });// closes router.get
 
-router.get('/:shortUrl', function(request, response){
-// var countClicks = function() { 
-//  db.shorten_url.find(
-//  {"shortened": request.param.shortUrl},
-//  function(err, results) {
-//    console.log(results);
-// }//closes function
-// )};//closes .find
+  router.get('/goto/:shortUrl', fucntion (request, respond) {
 
-//   db.shorten_url.update(
-//   {$inc {"clicks": 1}
-//   })//closes .update
-// };
-console.log('here')    // MongoClient.connect('mongodb://127.0.0.1:27017/shorten', function(err, db) {
- var collection = db.collection('shorten_url')
- // var shortKey = uuid.v4();
- var    shortUrl = request.params.shortUrl;
- console.log(shortUrl)
- // collection.find().toArray(function(err, url) {
-   collection.find(
-       {shortened: {$eq: shortUrl}},
-       { target: 1, shortened: 1}
-       ).toArray(function(err, result){
-         console.log(result)
-     // console.log(err)
-     // console.log(url)
-   response.render('post', {"url": result[0]});
- })
-     // })
-});
-});
-
-// db.collection.find(
-// {shortened:shortKey },
-// {
-//   target: 1
-// }
-//   ).limit(1)
+  })// closes router.get
+});//closes MongoClient
 
 
 module.exports = router;
